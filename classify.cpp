6e284ff4e69e9bae135e6badf87106ac7a68ed56
Yamm Elnekave <klavlavigugu@gmail.com>
2023-03-06 16:07:48 -0800

    classification random

--- classify.cpp
+++ classify.cpp
@@ -1,9 +1,17 @@
 
+#include <fstream>
 #include <iostream>
+#include <random>
+#include <sstream>
 #include <string>
 
-int random_classify(std::string input);
-int frequency_classify(std::string input);
+struct Author_Probability {
+    std::string author;
+    double probability;
+};
+
+Author_Probability random_classify(std::string input);
+Author_Probability frequency_classify(std::string input);
 void frequency_trainer(std::string* input, size_t size);
 std::string* read_file(std::string path);
 
@@ -18,15 +26,32 @@ int main(int argc, char** argv) {
 }
 
 std::string* read_file(std::string path) {
-    return nullptr;
+    // read a file and return its contents as a string
+    std::stringstream output;
+
+    std::ifstream infile(path);
+    std::string line;
+    while (std::getline(infile, line)) {
+        std::istringstream iss(line);
+
+        output << line << std::endl;
+    }
 }
 
-int random_classify(std::string input) {
-    return 0;
+Author_Probability random_classify(std::string input) {
+    srand(time(NULL));
+
+    double probability = (double)rand() / (double)RAND_MAX;
+
+    Author_Probability output;
+    output.probability = probability;
+    output.author = "Mark Twain";
+
+    return output;
 }
 
-int frequency_classify(std::string input) {
-    return 0;
+Author_Probability frequency_classify(std::string input) {
+    return Author_Probability();
 }
 
 void frequency_trainer(std::string* input, size_t size) {
