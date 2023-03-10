0db41b1c846bb8c3a717d07d04201bc8822e6319
nathanc7278 <cnatenate123@gmail.com>
2023-03-09 17:27:02 -0800

    fixed spaces in text cleanup

--- src/train.bash
+++ src/train.bash
@@ -1,1 +1,1 @@
-cat ../data/Authors/$1.txt | ./cleanup_scripts/new_lines_to_spaces.bash | ./train.exe -Author=$1
+cat ../data/Authors/$1.txt | ./cleanup_scripts/new_lines_to_@symbols.bash | ./train.exe -Author=$1
