1aa24fd85e1a8a17e665103cff81154fd7e0c602
nathanc7278 <cnatenate123@gmail.com>
2023-03-20 14:07:14 -0700

    train_model get_word_counts()

--- src/runner/train_model.cpp
+++ src/runner/train_model.cpp
@@ -7,27 +7,49 @@ and twain_vocab.txt
 
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 #include <string>
+#include <sstream>
 
 // make man
 
+std::unordered_map<std::string, int> get_word_counts(std::string file_name, int& total_number_of_words);
+ 
 int main(int argc, char** argv) {
     std::string author = argv[1];
-    size_t index = 8;
-    author = author.substr(index, author.size() - index);
+    author = author.substr(0, author.size());
 
     // uses cin stream to get each word
     // while (cin > word)
     // train the models, and write to correct files
 
     // write all of cin to text file
-    std::ofstream myfile;
-    myfile.open("temp/" + author + ".txt");
+    std::string file_name = "../../data/Authors/" + author + ".txt";
+    int total_number_of_words;
+    get_word_counts(file_name, total_number_of_words);
+    
 
+}
+
+std::unordered_map<std::string, int> get_word_counts(std::string file_name, int& total_number_of_words) {
+    std::unordered_map<std::string, int> hash_table_of_all_words;
     std::string word;
-    while (std::cin >> word) {
-        myfile << word << " hi";
+    std::ifstream myfile;
+    total_number_of_words = 0;
+    myfile.open(file_name, std::ifstream::in);     //change this directory to the clean data folder once we have it
+    if (myfile.fail()) 
+        std::cout << "File could not open." << std::endl;
+    while (!myfile.eof()) {
+        myfile >> word;
+        if (hash_table_of_all_words.find(word) == hash_table_of_all_words.end()) {
+                hash_table_of_all_words[word] = 1;
+        } else {
+                hash_table_of_all_words[word]++;
+        }
+        total_number_of_words++;
     }
+    myfile.close();
+    return hash_table_of_all_words;
 }
 
 // probabilties -> logarithmic probablities.
