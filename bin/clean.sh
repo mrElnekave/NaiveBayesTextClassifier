3d77d191fc421f53c7c9af54aea14f62987be1c1
nathanc7278 <cnatenate123@gmail.com>
2023-03-25 15:48:54 -0700

    organized directories

---
+++ bin/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cat $* \
+    | sed '1,/START/d' \
+    | tr A-Z a-z \
+    | tr -d '[",]' \
+    | sed -e 's/[.] */.~/g' \
+    | sed -e  's/[-—+]/ /g' \
+    | tr \\n ' ' \
+    | tr -d \\r \
+    | tr '~' '\n' \
+    | sed -e 's/[.?!:]/\n/g' \
+    | sed -e 's/[_/()\$#]//g' \
+    | sed -e 's/^ */<S> /' \
+    | sed -e 's# *$# </S>#' \
+    | sed -e 's/  */ /g' \
+    | sed -e 's/[^[:print:]]//g' \
+    | sed -e 's/www//g' \
+    | sed -e 's/pgdp//g' \
+    | sed -e 's/http//g' \
+    | grep -v '<S> *<\/S>' \
+    | tr -s ' ' \
+    | tr -s '\n' \
+    | awk 'NF > 5'
+
+# What does "awk 'NF > 5'" do?
